57f782240d7fe57f1a70318a37b0c93f271c90b6
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-08-13 18:40:50 +0530

    Reviewed security compliance.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoDb = async () => {
-    const URI = "mongodb+srv://learnbydoing00:yMkkenXED0NfJiMh@nodelearning.sel3b9f.mongodb.net/otpPractice";
+    const URI = process.env.DB_URL;
 
     await mongoose.connect(URI);
     console.log("Db connection done.");
